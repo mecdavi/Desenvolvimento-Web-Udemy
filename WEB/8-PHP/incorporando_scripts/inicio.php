8ecf8c5c5e38a9938401ce002bf705cc04243fb4
davideduardmc <david.eduard86@gmail.com>
2022-07-09 23:11:18 -0300

    aulas php

---
+++ WEB/8-PHP/incorporando_scripts/inicio.php
@@ -0,0 +1,9 @@
+<?php 
+    // inclui a pagina produzindo warning
+    include('menu.php');
+    include_once('menu.php');
+    require('menu.php');
+    require_once('menu.php');
+
+
+?>
---
+++ WEB/8-PHP/incorporando_scripts/menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Menu</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <style>
+            nav form div input{
+                width: 1100px;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+                <form action="">
+                        <input type="text" class="form-control"  placeholder="Pesquisar">
+                </form>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ml-5"><a class="nav-link" href="inicio.php"><i class="fa-solid fa-house-chimney fa-2x"></i></a></li>
+                    <li class="nav-item ml-5"><a class="nav-link" href="minha_rede.php"><i class="fa-solid fa-hospital-user fa-2x"></i></a></li>
+                    <li class="nav-item ml-5"><a class="nav-link" href="vagas.php"><i class="fa-solid fa-briefcase fa-2x"></i></a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
